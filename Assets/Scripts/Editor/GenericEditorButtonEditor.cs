b44595bce08657253bdb65ba8ad1f6b9ae6d0a33
Vojtech Bruza <vojtech.bruza@gmail.com>
2022-02-26 16:41:55 +0100

    Generic editor button

---
+++ Assets/Scripts/Editor/GenericEditorButtonEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(GenericEditorButton))]
+[CanEditMultipleObjects]
+public class GenericEditorButtonEditor : Editor
+{
+    GenericEditorButton myScript;
+    private void OnEnable()
+    {
+        myScript = (GenericEditorButton)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (GUILayout.Button("Invoke Events"))
+        {
+            myScript.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/GenericEditorButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GenericEditorButton : MonoBehaviour
+{
+    public UnityEvent onEditorButtonPress = new UnityEvent();
+
+    public void Invoke()
+    {
+        onEditorButtonPress?.Invoke();
+    }
+}
